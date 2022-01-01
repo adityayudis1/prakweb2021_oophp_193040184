ae67a284677bc5579e69f4175fe38de600792abb
Aditya Yudis <adityayudis4@gmail.com>
2022-01-01 12:10:23 +0700

    mengubah URL repo

--- pertemuan 1/index.php
+++ pertemuan 1/index.php
@@ -2,7 +2,7 @@
 /*
 Aditya Yudis I
 193040184
-https://github.com/adityayudis1/pw2022_193040184.git
+https://github.com/adityayudis1/prakweb2021_oophp_193040184/tree/main/pertemuan%201
 pertemuan 1 - 1 januari 2022
 Membuat URL git HUB
 */
