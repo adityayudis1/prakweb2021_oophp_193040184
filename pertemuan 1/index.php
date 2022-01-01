5ea17d2e36e58eb6deaa22baa258871588ad9fd6
Aditya Yudis <adityayudis4@gmail.com>
2022-01-01 08:16:31 +0700

    Menyelesaikan video pertemuan 1

---
+++ pertemuan 1/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Aditya Yudis I
+193040184
+https://github.com/adityayudis1/pw2022_193040184.git
+pertemuan 1 - 1 januari 2022
+Membuat URL git HUB
+*/
+?>
+<?php
+//standart output
+//echo, print
+?>
